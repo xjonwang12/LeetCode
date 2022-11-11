4882e3bb16254fe6065352e5a50c952c7e866f6e
xjonwang12 <117947186+xjonwang12@users.noreply.github.com>
2022-11-11 10:05:36 -0500

    Time: 28 ms (15.39%), Space: 10 MB (56.91%) - LeetHub

--- 0064-minimum-path-sum/0064-minimum-path-sum.cpp
+++ 0064-minimum-path-sum/0064-minimum-path-sum.cpp
@@ -4,9 +4,9 @@ public:
         int m=grid.size(), n=grid[0].size();
         vector<vector<int>> c(m, vector<int>(n,0));
         c[0][0]=grid[0][0];
-        for (int i=1; i<m; i++) c[i][0]=c[i-1][0]+grid[i][0];
-        for (int i=1; i<n; i++) c[0][i]=c[0][i-1]+grid[0][i];
-        for (int i=1; i<m; i++) for (int j=1; j<n; j++) c[i][j]=((c[i-1][j]<c[i][j-1])?c[i-1][j]:c[i][j-1]) + grid[i][j];
+        for (int i=1; i<m; i++) c[i][0] = c[i-1][0]+grid[i][0];
+        for (int i=1; i<n; i++) c[0][i] = c[0][i-1]+grid[0][i];
+        for (int i=1; i<m; i++) for (int j=1; j<n; j++) c[i][j] = ((c[i-1][j]<c[i][j-1]) ? c[i-1][j] : c[i][j-1]) + grid[i][j];
         return c[m-1][n-1];
     }
 };

a305355df4033266fec6d5a32f504cbc576fcaf3
xjonwang12 <117947186+xjonwang12@users.noreply.github.com>
2022-11-12 12:25:19 -0500

    Time: 150 ms (48.43%), Space: 41.5 MB (32.44%) - LeetHub

--- 1749-maximum-absolute-sum-of-any-subarray/1749-maximum-absolute-sum-of-any-subarray.cpp
+++ 1749-maximum-absolute-sum-of-any-subarray/1749-maximum-absolute-sum-of-any-subarray.cpp
@@ -2,12 +2,11 @@ class Solution {
 public:
     int maxAbsoluteSum(vector<int>& a) {
         int n=a.size(), c=abs(a[0]);
-        vector<pair<int, int>> dp(n,{0, 0});
-        dp[0]={(a[0]>0) ? a[0] : 0, (a[0]<0) ? a[0] : 0};
+        pair<int, int> dp = {(a[0]>0) ? a[0] : 0, (a[0]<0) ? a[0] : 0};
         for (int i=1; i<n; i++) {
-            dp[i].first = (dp[i-1].first+a[i] > a[i]) ? dp[i-1].first+a[i] : a[i];
-            dp[i].second = (dp[i-1].second+a[i] < a[i]) ? dp[i-1].second+a[i] : a[i];
-            c=max(c, max(dp[i].first, -1*dp[i].second));
+            dp.first = (dp.first+a[i] > a[i]) ? dp.first+a[i] : a[i];
+            dp.second = (dp.second+a[i] < a[i]) ? dp.second+a[i] : a[i];
+            c=max(c, max(dp.first, -1*dp.second));
         }
         return c;
     }

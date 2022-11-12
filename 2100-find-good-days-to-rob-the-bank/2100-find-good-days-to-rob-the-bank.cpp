601e7ca6c0c8f76396fd5d61316271d01d03a237
xjonwang12 <117947186+xjonwang12@users.noreply.github.com>
2022-11-12 13:50:49 -0500

    Time: 332 ms (46.14%), Space: 130.7 MB (5.32%) - LeetHub

---
+++ 2100-find-good-days-to-rob-the-bank/2100-find-good-days-to-rob-the-bank.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    vector<int> goodDaysToRobBank(vector<int>& s, int t) {
+        int n=s.size();
+        vector<vector<int>> dp(n, vector<int>(3, 0));
+        dp[0] = {s[0], 1, 1};
+        for (int i=1; i<n; i++) {
+            dp[i][0] = s[i];
+            dp[i][1] = (s[i]>=dp[i-1][0]) ? dp[i-1][1]+1 : 1;
+            dp[i][2] = (s[i]<=dp[i-1][0]) ? dp[i-1][2]+1 : 1;
+            /*
+            for (auto x:dp[i]) {
+                cout << x << " ";
+            }
+            cout << endl;
+            */
+        }
+        vector<int> a;
+        for (int i=t; i<n-t; i++) {
+            if (dp[i][2]>t && dp[i+t][1]>t) {
+                a.push_back(i);
+            }
+        }
+        return a;
+    }
+};

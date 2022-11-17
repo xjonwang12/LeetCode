f3d9e20e09fe164e15102afedbb9924352a09dc7
xjonwang12 <117947186+xjonwang12@users.noreply.github.com>
2022-11-17 17:28:19 -0500

    Time: 5 ms (34.27%), Space: 5.8 MB (92.77%) - LeetHub

--- 0790-domino-and-tromino-tiling/0790-domino-and-tromino-tiling.cpp
+++ 0790-domino-and-tromino-tiling/0790-domino-and-tromino-tiling.cpp
@@ -1,12 +1,12 @@
 class Solution {
 public:
     int numTilings(int n) {
-        vector<long long> dp1(n+1, 0), dp2(n+1, 0);
-        dp1[0]=1, dp1[1]=1, dp2[0]=1;
+        long long dp1=1, dp2=1, dp3=0;
         for (int i=2; i<=n; i++) {
-            dp1[i]=(dp1[i-1]+dp1[i-2]+dp2[i-1]) % ((int)1e9+7);
-            dp2[i]=(2*dp1[i-2]+dp2[i-1]) % ((int)1e9+7);
+            long long t1=(dp2+dp1+dp3) % ((int)1e9+7);
+            dp3=(2*dp1+dp3) % ((int)1e9+7);
+            dp1=dp2, dp2=t1;
         }
-        return dp1[n];
+        return dp2;
     }
 };

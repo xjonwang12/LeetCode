0dc99ee6a15e544e4d0cdda69405b2e829b9f002
xjonwang12 <117947186+xjonwang12@users.noreply.github.com>
2022-11-19 22:46:43 -0500

    Time: 22 ms (69.06%), Space: 11.8 MB (62.29%) - LeetHub

--- 0797-all-paths-from-source-to-target/0797-all-paths-from-source-to-target.cpp
+++ 0797-all-paths-from-source-to-target/0797-all-paths-from-source-to-target.cpp
@@ -1,17 +1,20 @@
 class Solution {
 public:
+    int d;
+    vector<vector<int>> ans;
+    vector<int> prev;
     vector<vector<int>> allPathsSourceTarget(vector<vector<int>>& g) {
-        vector<vector<int>> ans;
-        dfs(g, 0, g.size()-1, ans, {0});
+        d=g.size()-1;
+        dfs(g, 0);
         return ans;
     }
     
-    void dfs(vector<vector<int>>& g, int s, int d, vector<vector<int>>& ans, vector<int> prev) {
+    void dfs(vector<vector<int>>& g, int s) {
+        prev.push_back(s);
         if (s==d) ans.push_back(prev);
         for (auto v : g[s]) {
-            prev.push_back(v);
-            dfs(g, v, d, ans, prev);
-            prev.pop_back();
+            dfs(g, v);
         }
+        prev.pop_back();
     }
 };

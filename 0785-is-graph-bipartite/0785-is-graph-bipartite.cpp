c4bae508b5a7daaf52d9ff65bf566d5c310ce130
xjonwang12 <117947186+xjonwang12@users.noreply.github.com>
2022-11-22 22:03:49 -0500

    Time: 69 ms (21.02%), Space: 13.6 MB (59.74%) - LeetHub

--- 0785-is-graph-bipartite/0785-is-graph-bipartite.cpp
+++ 0785-is-graph-bipartite/0785-is-graph-bipartite.cpp
@@ -2,22 +2,20 @@ class Solution {
 public:
     int n;
     vector<short> vis;
-    vector<vector<int>> g;
-    bool isBipartite(vector<vector<int>>& graph) {
-        n=graph.size();
-        g=graph;
+    bool isBipartite(vector<vector<int>>& g) {
+        n=g.size();
         vis=vector<short>(n, 0);
         for (int i=0; i<n; i++) {
-            if (!vis[i] && !dfs(i, -1)) return false;
+            if (!vis[i] && !dfs(g, i, -1)) return false;
         }
         return true;
     }
     
-    bool dfs(int c, int l) {
+    bool dfs(vector<vector<int>>& g, int c, int l) {
         vis[c]=-l;
         for (auto u : g[c]) {
             if (vis[u]==vis[c]) return false;
-            if (!vis[u] && !dfs(u, vis[c])) return false;
+            if (!vis[u] && !dfs(g, u, vis[c])) return false;
         }
         return true;
     }

2eab41bb5cbf5061c294a9e853178c8823e738e6
xjonwang12 <117947186+xjonwang12@users.noreply.github.com>
2022-11-28 19:17:53 -0500

    Time: 734 ms (5.06%), Space: 71.9 MB (46.84%) - LeetHub

---
+++ 0687-longest-univalue-path/0687-longest-univalue-path.cpp
@@ -0,0 +1,31 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int a=0;
+    int longestUnivaluePath(TreeNode* root) {
+        dfs(root);
+        return a;
+    }
+    
+    int dfs(TreeNode* curr) {
+        if (!curr) return 0;
+        int l=0, r=0;
+        l = dfs(curr->left);
+        r = dfs(curr->right);
+        l = (curr-> left && curr->left->val == curr->val) ? l : 0;
+        r = (curr->right && curr->right->val == curr->val) ? r : 0;
+        a=max(a, l+r);
+        cout << 1 + max(l, r) << endl;
+        return 1 + max(l, r);
+    }
+};
